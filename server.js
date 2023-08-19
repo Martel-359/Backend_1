094dffd3242b2920fd93adb5212cb8411e840812
Martel-359 <doanhuuvinh0108@gmail.com>
2023-08-19 16:12:06 +0700

    cai dat handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-const app  = require ("./app");
+const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
 
-const port = config.app.port;
-app.listen(port,()=>{
-    console.log(`Sever is running on port ${port}.`);
-});
+
+
+async function startServer() {
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("connect to database!");
+        const port = config.app.port;
+        app.listen(port, () => {
+            console.log(`Sever is running on port ${port}.`);
+        });
+
+    } catch (error) {
+        console.log("Cannot connect to database!", error);
+        process.exit();
+    }
+}
+
+startServer();
